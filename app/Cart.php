c748d8cdd4b85fe4328b330a3fb2860ad077c3a0
macintosh <aliyeketaz@gmail.com>
2018-01-25 21:07:31 +0330

    updated

--- app/Cart.php
+++ app/Cart.php
@@ -4,11 +4,26 @@ namespace App;
 
 use App\Models\Factor;
 use Illuminate\Database\Eloquent\Model;
+use Morilog\Jalali\jDate;
 
 class Cart extends Model
 {
     protected $table = 'carts';
     protected $fillable = ['product_id','customer_id','qty','video_id','vote_id','vote_p','status'];
+
+
+    public function getCreatedAtAttribute($value)
+    {
+
+        return jDate::forge($value)->format('%d %B، %Y / ساعت : H:i');
+    }
+
+
+    public function getUpdatedAtAttribute($value)
+    {
+
+        return jDate::forge($value)->format('%d %B، %Y / ساعت : H:i');
+    }
 
     public function factors()
     {
